f4d8cd3f8a8262a2fa6ee7fab22de0f2894927b0
Jordan Pisaniello <jpisaniello@pm.me>
2021-02-13 22:41:25 -0500

    typo

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo systemctl start sshd
 sudo dnf install ansible git -y
-mkdir roles
-git clone https://github.com/luyz25/gnome_extensions.git roles/gnome_extensions
+mkdir roles && git clone https://github.com/jaredhocutt/ansible-gnome-extensions roles/gnome_extensions
